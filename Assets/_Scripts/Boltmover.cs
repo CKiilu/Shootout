fbf919f55021c2d0183d12829bf6cb996473b6f3
CKiilu <cndeti@gmail.com>
2016-09-24 19:27:39 +0300

    Move bullets

---
+++ Assets/_Scripts/Boltmover.cs
@@ -0,0 +1,15 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Boltmover : MonoBehaviour {
+
+    public float speed;
+    private Rigidbody bolt;
+
+    void Start()
+    {
+        bolt = GetComponent<Rigidbody>();
+
+        bolt.velocity = transform.forward * speed;
+    }
+}
