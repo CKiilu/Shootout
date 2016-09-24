62e50c8f5cc633ededf61c42c0821e44341f910c
CKiilu <cndeti@gmail.com>
2016-09-24 20:54:25 +0300

    added collidables

---
+++ Assets/_Scripts/RandomRotator.cs
@@ -0,0 +1,13 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class RandomRotator : MonoBehaviour {
+
+    public float tumble;
+    private Rigidbody asteroid;
+
+	void Start () {
+        asteroid = GetComponent<Rigidbody>();
+        asteroid.angularVelocity = Random.insideUnitSphere * tumble;
+    }
+}
